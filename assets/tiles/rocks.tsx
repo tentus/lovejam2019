d790b76f0d6c6ae2aea1a6f3fa5969218e575aea
Thomas Holden <tentus@gmail.com>
2019-02-17 13:05:58 -0500

    rough in a set of art assets and maps

---
+++ assets/tiles/rocks.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="rocks" tilewidth="64" tileheight="64" tilecount="16" columns="4">
+ <image source="rocks.png" width="256" height="256"/>
+ <terraintypes>
+  <terrain name="rock" tile="5"/>
+ </terraintypes>
+ <tile id="5" terrain=",,,0"/>
+ <tile id="6" terrain=",,0,0"/>
+ <tile id="7" terrain=",,0,"/>
+ <tile id="9" terrain=",0,,0"/>
+ <tile id="10" terrain="0,0,0,0"/>
+ <tile id="11" terrain="0,,0,"/>
+ <tile id="13" terrain=",0,,"/>
+ <tile id="14" terrain="0,0,,"/>
+ <tile id="15" terrain="0,,,"/>
+</tileset>
---
+++ assets/tiles/stone-blocks.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="stone-blocks" tilewidth="64" tileheight="64" tilecount="20" columns="5">
+ <image source="stone-blocks.png" width="320" height="256"/>
+</tileset>
